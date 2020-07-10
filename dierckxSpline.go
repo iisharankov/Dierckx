549845882acb09cc812cd2ddbfc2a660efe4bee5
iisharankov <ivansharankov3@gmail.com>
2020-07-10 10:25:25 -0400

    Initial push to new repository

---
+++ dierckxSpline.go
@@ -0,0 +1,185 @@
+package dierckx
+
+// #cgo FFLAGS: -fdefault-real-8
+// void splev (double*, int*, double*, int*, double*, double*, int*, int*);
+// void curfit (int*, int*, double*, double*, double*, double*, double*, int*, double*, int*, int*, double*, double*, double*, double*, int*, int*, int*);
+import "C"
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Spline1D takes in x and y arrays, and ports the data to Direckx to return a spline
+func Spline1D(x, y []float64, k int) ([]float64, []float64, error) {
+
+	/*
+		Spline1D ports the given data so it can be ran by the included FORTRAN dierckx library. To do this, we heavily use
+		the C library to convert all types to C types and pass all the references to the FORTRAN code. This function is truly
+		jut a port from Golang to FORTRAN to use the required FORTRAN function.
+
+		If you want detailed descriptions of the variables ported to the FORTRAN library, many of them contain the same names,
+		and can be expanded by reading the very well documented FORTRAN docstring on the input types.
+	*/
+
+	// Used for returning arrays durring errors
+	var emptyArray []float64
+
+	/*
+		The x and y parameters are arrays of float64, while the C compiler that ports to FORTRAN is an array of C.double.
+		Obviously, this should not be an issue, since other than the name, these two types are identical in every way. But
+		Golang is a bit too picky and even this needs to be converted. This is done by making an array of C.doubles and
+		manually appending each element in x and y.
+	*/
+	copyOfX := make([]C.double, len(x))
+	copyOfY := make([]C.double, len(y))
+
+	// w is an array of ones of same length as the input arrays. This is done in the for loop
+	w := make([]C.double, len(x))
+
+	for i := range x {
+		copyOfX[i] = C.double(x[i])
+		copyOfY[i] = C.double(y[i])
+		w[i] = 1
+	}
+
+	// Depreciated parameters left for possible later use
+	// var periodic bool = false
+	// var bc string = "nearest"
+
+	var s float64 = 0.0
+	var m int = len(x)
+
+	if len(y) != m {
+		return emptyArray, emptyArray, errors.New("length of x and y must match")
+	}
+	if len(w) != m {
+		return emptyArray, emptyArray, errors.New("length of x and w must match")
+	}
+	if !(m > k) {
+		return emptyArray, emptyArray, errors.New("k must be less than length(x)")
+	}
+	if !(1 <= k) || !(k <= 5) {
+		return emptyArray, emptyArray, errors.New("1 <= k = $k <= 5 must hold")
+	}
+
+	var nest int = MaxOf(m+k+1, (2*k)+3)
+	var lwrk int = m*(k+1) + nest*(7+(3*k))
+
+	var wrk = make([]C.double, lwrk)
+	var iwrk = make([]C.int, nest)
+	var varOfZero C.int = 0
+
+	// outputs
+	var n int32
+	var t = make([]C.double, nest)
+	var c = make([]C.double, nest)
+	var fp float64
+	var ier int32
+
+	// Cast all the above variables created to their C counterpart
+
+	castOfm := C.int(m)
+	castOfk := C.int(k)
+	castOfs := C.double(s)
+	castOfnest := C.int(nest)
+	castOfn := C.int(n)
+	castOffp := C.double(fp)
+	castOflwrk := C.int(lwrk)
+	castOfier := C.int(ier)
+
+	// C.curfit is the name call given to the FORTRAN function that calculates the spline knots and coefficients
+	// a := []C.double{0, m, x, y, w, x[0], x[len(x)-1], k, s, nest, n, t, c, fp, wrk, lwrk, iwrk, ier}
+	C.curfit(&varOfZero, &castOfm, &copyOfX[0], &copyOfY[0], &w[0], &copyOfX[0], &copyOfX[len(x)-1], &castOfk, &castOfs, &castOfnest, &castOfn, &t[0], &c[0], &castOffp, &wrk[0], &castOflwrk, &iwrk[0], &castOfier) // pass addresses
+	/*
+		The above call populated the t and c variables, which now contain the location of all the knots in the spline, and the
+		coefficients (respectively). These two arrays are of type []_Ctype_double, so naturally we must convert them back to
+		[]float64 as the inverse of what we did at the beginning of the function.
+	*/
+	// Change back from list of []_Ctype_double to []float64
+
+	if castOfier != -1 {
+		fmt.Println("Fortran compiled with error of:", castOfier)
+	}
+
+	locOfKnots := make([]float64, len(t))
+	coefficients := make([]float64, len(c))
+
+	for i := range t {
+		locOfKnots[i] = float64(t[i])
+		coefficients[i] = float64(c[i])
+	}
+
+	// Return the arrays
+	return locOfKnots, coefficients, nil
+}
+
+//////////////////////////////////////////////////////////////////
+func Evaluate(spline, coefficients, xValues []float64, k int) []float64 {
+
+	CCompatableSpline := make([]C.double, len(spline))
+	CCompatableXValues := make([]C.double, len(xValues))
+	CCompatableCoefficients := make([]C.double, len(coefficients))
+
+	for i := range xValues {
+		CCompatableXValues[i] = C.double(xValues[i])
+	}
+
+	for i := range spline {
+		CCompatableSpline[i] = C.double(spline[i])
+		CCompatableCoefficients[i] = C.double(coefficients[i])
+	}
+
+	numOfKnots := C.int(len(spline))
+
+	degree := C.int(k)
+	var m C.int = C.int(len(xValues))
+
+	// // outputs
+	var y = make([]C.double, len(xValues))
+	var ier int32
+	castOfier := C.int(ier)
+
+	// Change back from list of []_Ctype_double to []float64
+	listOfYValues := make([]float64, len(y))
+
+	C.splev(&CCompatableSpline[0], &numOfKnots, &CCompatableCoefficients[0], &degree, &CCompatableXValues[0], &y[0], &m, &castOfier) // pass addresses
+	// for i := range xValues {
+	// 	// TODO If I use package unsaffe to somehow pass the full array, I don't need to call fortran len(xValues) times!
+	// 	C.splev(&CCompatableSpline[0], &numOfKnots, &CCompatableCoefficients[0], &degree, &CCompatableXValues[i], &y[i], &m, &castOfier) // pass addresses
+	// }
+	for i := range listOfYValues {
+		// Convert _Ctype_double to float64  and append to new list that can store it
+		listOfYValues[i] = float64(y[i])
+	}
+
+	return listOfYValues
+}
+
+// MaxOf returns the max parameter passed in
+func MaxOf(vars ...int) int {
+
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+
+// Fib returns the nth number in the Fibonacci series.
+// func Fib(n int) int {
+// 	if n < 2 {
+// 			return n
+// 	}
+// 	return Fib(n-1) + Fib(n-2)
+// }
+
+func Temp(n []int) int {
+	if len(n) > 3 {
+		return len(n)
+	}
+	return 0
+}
---
+++ dierckxSpline_test.go
@@ -0,0 +1,227 @@
+package dierckx
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"testing"
+)
+
+func outputToTxt(name string, value []float64) {
+	// Open Azimuth file to write to
+	AzFile, err := os.Create(name + ".txt")
+	if err != nil {
+		log.Println(err)
+	}
+	defer AzFile.Close()
+
+	for i := range value {
+		AzData := fmt.Sprintf("%v\n", value[i])
+		_, err = io.WriteString(AzFile, AzData)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+type Spline1DData struct {
+	filename string
+	azPoints []float64
+	timeVals []float64
+}
+
+var splineTestTable = []Spline1DData{
+	// {"TestData1", []float64{80, 82, 85, 84, 82, 80}, []float64{1592771864, 1592771874, 1592771884, 1592771889, 1592771894, 1592771904}},
+	{"TestData2", []float64{40, 70, 90, 150, 160, 200, 80, 30, 50, 80}, []float64{0, 50, 75, 140, 150, 200, 300, 350, 450, 500}},
+	{"TestData3", []float64{80, 40, 80, 120, 80, 40, 80, 120, 80}, []float64{0, 50, 100, 150, 200, 250, 300, 350, 400}},
+	// {"TestData4", []float64{80, 40, 80}, []float64{0, 100, 200}},
+}
+
+
+func TestSpline1D(t *testing.T) {
+
+	// actual := Temp(tt.n)
+	// if actual != tt.expected {
+	// 	t.Errorf("Fib(%d): expected %d, actual %d", tt.n, tt.expected, actual)
+	// }
+
+	/*
+		The below azPoints and timeVals was sent to the Julia package and is saved as JuliaOutput.txt.
+		Send the same command to the two functions necessary to get the same spline and see if all the
+		values of the two text files are identical
+	*/
+
+	for _, tt := range splineTestTable {
+		// azPoints := []float64{80, 82, 85, 84, 82, 80}
+		// timeVals := []float64{1592771864, 1592771874, 1592771884, 1592771889, 1592771894, 1592771904}
+
+		azPoints := tt.azPoints
+		timeVals := tt.timeVals
+		fmt.Println(azPoints)
+
+		var k int
+		if len(azPoints) >= 4 {
+			k = 3
+		} else if len(azPoints) == 3 {
+			k = 2
+		} else if len(azPoints) == 2 {
+			k = 1
+		}
+		fmt.Println(k)
+		locOfKnots, coefficients, err := Spline1D(timeVals, azPoints, k)
+		if err != nil {
+			// ErrorMsg := fmt.Sprintf("Spline1D returned an unexpected error: %v", err)
+			// t.Errorf(ErrorMsg)
+			return
+		}
+
+		// Need to Evaluate() over the locOfKnots at the same step size as Julia
+		start := timeVals[0]
+		end := timeVals[len(timeVals)-1]
+		step := 1.0 / 20.0
+		xValues := []float64{}
+		for i := start; i < end; i += step {
+			xValues = append(xValues, i)
+		}
+
+		// Send the xValues over to evaluate at each x point point
+		yVals := Evaluate(locOfKnots, coefficients, xValues, 3)
+
+		// Print out to text file, then load it along side the Julia text file
+		outputToTxt("Test_Output", yVals)
+
+		file, err := os.Open("Test_Output.txt")
+		defer file.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		juliaFile, err := os.Open(tt.filename + ".txt")
+		defer juliaFile.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Read each line of the two text files and make sure they're identical
+		i := 0
+		for {
+			i ++
+			var julia float64
+			var test float64
+
+			var n int
+			n, err = fmt.Fscanln(juliaFile, &julia)
+			if n == 0 || err != nil {
+				break
+			}
+
+			n, err = fmt.Fscanln(file, &test)
+			if n == 0 || err != nil {
+				break
+			}
+
+			// Floating point rounding, so make sure abs difference is very low
+			if math.Abs(julia-test) > 0.00000001 {
+				ErrorMsg := fmt.Sprintf("%v: The created spline was not identical to the Julia spline: %v", i, math.Abs(julia-test))
+				t.Errorf(ErrorMsg)
+				// continue
+			}
+		}
+	}
+}
+
+// func TestSpline1DONLYONE(t *testing.T) {
+
+// 	/*
+// 		The below azPoints and timeVals was sent to the Julia package and is saved as JuliaOutput.txt.
+// 		Send the same command to the two functions necessary to get the same spline and see if all the
+// 		values of the two text files are identical
+// 	*/
+// 	azPoints := []float64{80, 82, 85, 84, 82, 80}
+// 	timeVals := []float64{1592771864, 1592771874, 1592771884, 1592771889, 1592771894, 1592771904}
+
+// 	locOfKnots, coefficients, err := Spline1D(timeVals, azPoints, 3)
+// 	if err != nil {
+// 		ErrorMsg := fmt.Sprintf("Spline1D returned an unexpected error: %v", err)
+// 		t.Errorf(ErrorMsg)
+// 	}
+
+// 	// Need to Evaluate() over the locOfKnots at the same step size as Julia
+// 	start := timeVals[0]
+// 	end := timeVals[len(timeVals)-1]
+// 	step := 1.0 / 20.0
+// 	xValues := []float64{}
+// 	for i := start; i < end; i += step {
+// 		xValues = append(xValues, i)
+// 	}
+
+// 	// Send the xValues over to evaluate at each x point point
+// 	yVals := Evaluate(locOfKnots, coefficients, xValues, 3)
+
+// 	// Print out to text file, then load it along side the Julia text file
+// 	outputToTxt("Test_Output", yVals)
+
+// 	file, err := os.Open("Test_Output.txt")
+// 	defer file.Close()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	juliaFile, err := os.Open("JuliaOutput_1.txt")
+// 	defer juliaFile.Close()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	// Read each line of the two text files and make sure they're identical
+// 	i := 0
+// 	for {
+// 		i++
+// 		var julia float64
+// 		var test float64
+
+// 		var n int
+// 		n, err = fmt.Fscanln(juliaFile, &julia)
+// 		if n == 0 || err != nil {
+// 			break
+// 		}
+
+// 		n, err = fmt.Fscanln(file, &test)
+// 		if n == 0 || err != nil {
+// 			break
+// 		}
+// 		fmt.Println(math.Abs(julia-test))
+// 		// fmt.Println(math.Abs(julia-test))
+// 		// Floating point rounding, so make sure abs difference is very low
+// 		if math.Abs(julia-test) > 0.000001 {
+// 			ErrorMsg := fmt.Sprintf("%v: The created spline was not identical to the Julia spline: %v", i, err)
+// 			t.Errorf(ErrorMsg)
+// 			// break
+// 		}
+
+// 	}
+// }
+
+func lineCounter(r io.Reader) (int, error) {
+	buf := make([]byte, 32*1024)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := r.Read(buf)
+		count += bytes.Count(buf[:c], lineSep)
+
+		switch {
+		case err == io.EOF:
+			return count, nil
+
+		case err != nil:
+			return count, err
+		}
+
+	}
+}
+
